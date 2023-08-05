a60fd330bed8b5f16856805ddce36e47f8790506
Vaibhav Nigam <lazyvab@gmail.com>
2023-08-05 13:48:03 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (message, sender) {
   if (message.action === "NOTIFY_CONTENT_SCRIPT_LOADED") {
-    console.log("### Acknowledged notification from content script in page: ", sender.tab.url);
-    sendResponse();
+    console.log(`### ACK content script in page: ${message.pageURL} and tab: ${sender.tab.url}`);
+    chrome.tabs.sendMessage(sender.tab.id, { action: "MESSAGE_FROM_BG", payload: "Hello world" });
   }
 });
