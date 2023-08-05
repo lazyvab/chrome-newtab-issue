df5426a7c1767d73837569f45df1b0a8bf835dd5
Vaibhav Nigam <lazyvab@gmail.com>
2023-08-05 13:48:21 +0530

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,7 +1,8 @@
-if (window.top === window) {
-  console.log("### Notifying background from page: " + location.href);
-  
-  chrome.runtime.sendMessage({ action: "NOTIFY_CONTENT_SCRIPT_LOADED" }, () => {
-    console.log("### Notified background from page: " + location.href);
-  });
-}
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === "MESSAGE_FROM_BG") {
+    console.log("### Received message from background", message.payload);
+  }
+});
+
+console.log("### Notifying background from page: " + location.href);
+chrome.runtime.sendMessage({ action: "NOTIFY_CONTENT_SCRIPT_LOADED", pageURL: location.href });
